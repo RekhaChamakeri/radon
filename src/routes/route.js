74cf6eb0b9c3af42ab9c9e2e34b6723a5b4ded7b
rekhachamakeri <rekhagchamakeri@gmail.com>
2022-06-03 09:50:34 +0530

    assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,36 +1,78 @@
 const express = require('express');
-const logger = require('../logger/logger.js')
-const helper = require('../util/helper.js')
-const validator = require('../validator/formatter.js')
+const { indexOf } = require('lodash');
+const lodash = require('lodash');
 
 const router = express.Router();
+//....................................................nodejs module assignment problem no:4....................................................
+router.get('/hello', function (req, res) {
+    const data = ["jan", "feb", "march", "apr", "may", "june", "july", "august", "sept", "oct", "nov", "dec"];
+    const chunks = lodash.chunk(data, 4);
+    console.log(chunks);
 
-router.get('/test-me', function (req, res) {
-    //console.log('The constant in logger route has a value '+externalModule.endpoint)
-    //console.log('The current batch is '+externalModule.batch)
-    logger.welcome()
-    console.log("date " +helper.printDate)
-    console.log("month " +helper.printMonth)
-    helper.getBatchInfo()
-    validator.formatter()
-    res.send('My first ever api!')
+    const oddNum =[1,3,5,7,9,11,13,15,17,19]
+    const lastElements = lodash.tail(oddNum);
+    console.log(lastElements)
+
+    const arr1=[1,4,2,4,6]
+    const arr2=[6,12,18,3]
+    const arr3=[1,8,6,22,30]
+    const arr4=[8,5,22,26,13]
+    const arr5=[40,33,5,27,15]
+    const mergedArray=lodash.union(arr1,arr2,arr3,arr4,arr5)
+    console.log(mergedArray)
+
+    let pairs = [["horror","The Shining"],["drama","Titanic"],["thriller","Shutter Island"],["fantasy","Pans Labyrinth"]]
+    let obj = lodash.fromPairs(pairs);
+    console.log(obj)
+
+   res.send('Hello there,Its my API')
+ });
+
+//........................................................ 1 .................................................................
+ router.get('/param', function (req, res) {
+   const movies=["3Idiots","kgf","RRR","googli"]
+   res.send(movies)
 });
 
-// router.get('/test-me1', function (req, res) {
-//     res.send('My second ever api!')
-// });
-
-// router.get('/test-me2', function (req, res) {
-//     res.send('My third api!')
-// });
-
-// router.get('/test-me3', function (req, res) {
-//     res.send('My 4th api!')
-// });
-
-router.get('/test-me4', function (req, res) {
-    res.send('My last api!')
-});
+//.........................................................2 and 3...................................................................
+router.get('/movies/:indexNumber', function (req, res) {
+  const movies=["3Idiots","kgf","RRR","googli"]
+  JSON.stringify(req.params)
+  let len=movies.length
+  let movieName=movies[req.params.indexNumber]
+  if(req.params.indexNumber<len){
+    res.send(movieName)
+ }
+ else{
+res.send("error: there is no such movie")
+ }
+ });
+ 
+//.....................................................................4..................................................
+ router.get('/film/:filmId', function (req, res) {
+   const film=[ {
+    "id": 1,
+    "name": "The Shining"
+   }, {
+    "id": 2,
+    "name": "Incendies"
+   }, {
+    "id": 3,
+    "name": "Rang de Basanti"
+   }, {
+    "id": 4,
+    "name": "Finding Nemo"
+   }]
+   JSON.stringify(req.params)
+   const len= film.length
+   const filmObj=film[req.params.filmId]
+   if(req.params.filmId<len){
+    res.send(filmObj)
+ }
+ else{
+res.send(film)
+ }
+    
+  });
 
 module.exports = router;
-// adding this comment for no reason
