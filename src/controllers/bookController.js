1a4fb6037ef44775e0ae59f7bbdcdfbc3100df2b
rekhachamakeri <rekhagchamakeri@gmail.com>
2022-06-07 10:59:52 +0530

    assbooks

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,5 +1,7 @@
 const { count } = require("console")
 const BookModel= require("../models/bookModel")
+//...............................................................01.......................................
+//createBook : to create a new entry..use this api to create 11+ entries in your collection
 
 const createBook= async function (req, res) {
     let data= req.body
@@ -8,13 +10,64 @@ const createBook= async function (req, res) {
     res.send({msg: savedData})
 }
 
-const getBooksData= async function (req, res) {
 
-    // let allBooks= await BookModel.find( ).count() // COUNT
+//.................................................................02................................
+//bookList : gives all the books- their bookName and authorName only 
 
-    // let allBooks= await BookModel.find( { authorName : "Chetan Bhagat" , isPublished: true  } ) // AND
-    
-    // let allBooks= await BookModel.find( { 
+const bookList= async function (req, res) {
+    let allBooks= await BookModel.find({},["bookName","authorName"])
+    res.send({msg: allBooks})
+}
+
+
+
+//................................................................03...........................................
+//getBooksInYear: takes year as input in post request and gives list of all books published that year
+
+const getBooksInYear= async function (req, res) {
+    JSON.stringify(req.params)
+    let a=req.params.inputYear
+    let allBooks= await BookModel.find( {year: {$eq:a}} ) // AND
+    res.send({msg: allBooks})
+}
+
+
+//...................................................................04.............................................
+//getBooksInYear: takes year as input in post request and gives list of all books published that year
+// getParticularBooks:- (this is a good one, make sincere effort to solve this) take any input and use it as a condition to fetch books that satisfy that condition
+// e.g if body had { name: “hi”} then you would fetch the books with this name
+// if body had { year: 2020} then you would fetch the books in this year
+// hence the condition will differ based on what you input in the request body
+
+const getParticularBooks= async function (req, res) {
+    let allBooks= await BookModel.find(req.query) 
+    res.send({msg: allBooks})
+}
+
+
+
+//.........................................................................05..............................................
+//getXINRBooks- request to return all books who have an Indian price tag of “100INR” or “200INR” or “500INR” 
+
+const getXINRBooks= async function (req, res) {
+    let allBooks= await BookModel.find( { 
+             $or: [ {"prices.indianPrice" : "100INR" } , { "prices.indianPrice" : "200INR" } , {  "prices.indianPrice" : "500INR" }]
+         } ) // AND
+   res.send({msg: allBooks})
+ }
+   
+
+
+//.........................................................................06................................................
+//getRandomBooks - returns books that are available in stock or have more than 500 pages 
+
+const getRandomBooks= async function (req, res) {
+    let allBooks= await BookModel.find( { 
+            $or: [{ stockAvailable : true } , {  "totalPages": {$gt:500} }]} ) 
+    res.send({msg: allBooks})
+}
+   
+// let allBooks= await BookModel.find( { 
     //     $or: [ {authorName : "Chetan Bhagat" } , { isPublished: true } , {  "year": 1991 }]
     // } ).select( { bookName: 1, authorName: 1, _id: 0})n // SELECT keys that we want
 
@@ -65,21 +118,20 @@ const getBooksData= async function (req, res) {
     
     // ASYNC AWAIT
     
-    let a= 2+4
-    a= a + 10
-    console.log(a)
-    let allBooks= await BookModel.find( )  //normally this is an asynchronous call..but await makes it synchronous
+    
+    //let allBooks= await BookModel.find( )  //normally this is an asynchronous call..but await makes it synchronous
 
 
     // WHEN AWAIT IS USED: - database + axios
     //  AWAIT can not be used inside forEach , map and many of the array functions..BE CAREFUL
-    console.log(allBooks)
-    let b = 14
-    b= b+ 10
-    console.log(b)
-    res.send({msg: allBooks})
-}
+    
+    
+ 
 
 
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+module.exports.bookList= bookList
+module.exports.getBooksInYear= getBooksInYear
+module.exports.getParticularBooks= getParticularBooks
+module.exports.getXINRBooks= getXINRBooks
+module.exports.getRandomBooks= getRandomBooks
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,20 +1,26 @@
 const mongoose = require('mongoose');
 
 const bookSchema = new mongoose.Schema( {
-    bookName: String, 
+    bookName: {
+        type: String, 
+        required: true
+    },
     authorName: String, 
     tags: [String],
-    
-    isPublished: Boolean,
     prices: {
         indianPrice: String,
         europePrice: String,
     },
-    sales: {type: Number, default: 10}
+    year:{
+        type: Number,
+        default:2022
+    },
+    totalPages: Number,
+    stockAvailable:Boolean
 }, { timestamps: true });
 
 
-module.exports = mongoose.model('Book', bookSchema) //users
+module.exports = mongoose.model('Book2', bookSchema) //users
 
 //Validation:
 //require:true
