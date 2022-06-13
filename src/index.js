4793d7356ceab5dcae655daddf686c6ef2d257bb
rekhachamakeri <rekhagchamakeri@gmail.com>
2022-06-13 19:42:16 +0530

    msg

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzotr.mongodb.net/Pritesh8769811-DB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Adhish-1998-DataBase:vQrIj9jTyDzRssqt@cluster0.af5tq.mongodb.net/RekhaChamakeri?retryWrites=true&w=majority", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
--- src/middlewares/commonMiddlewares.js
+++ src/middlewares/commonMiddlewares.js
@@ -1,26 +1,45 @@
+const orderModel = require("../models/orderModel")
+const userModel = require("../models/userModel")
+const productModel = require("../models/productModel")
 
 const mid1= function ( req, res, next) {
-    req.falana= "hi there. i am adding something new to the req object"
-    console.log("Hi I am a middleware named Mid1")
-    next()
+    let hdata=req.headers.isfreeappuser
+    if(hdata){
+    console.log("isFreeAppUser is present")
+    next()}
+    else{res.send({msg: "the request is missing a mandatory header"})}
 }
 
-const mid2= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid2")
-    next()
+const mid2= async function ( req, res, next) {
+    order=req.body
+    userId= await userModel.findById(order.userId)
+    if(userId){
+        console.log("user exists")
+        next()
+    }
+   else{
+    res.send({msg:"user not exist"})
+   }
 }
 
-const mid3= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid3")
-    next()
+ const mid3= async function ( req, res, next) {
+    product=req.body
+    productId= await productModel.findById(product.productId)
+    if(productId){
+        console.log("product exists")
+        next()
+    }
+   else{
+    res.send({msg:"product not exist"})
+   }
 }
 
-const mid4= function ( req, res, next) {
-    console.log("Hi I am a middleware named Mid4")
-    next()
-}
+// const mid4= function ( req, res, next) {
+//     console.log("Hi I am a middleware named Mid4")
+//     next()
+// }
 
 module.exports.mid1= mid1
 module.exports.mid2= mid2
 module.exports.mid3= mid3
-module.exports.mid4= mid4
+// module.exports.mid4= mid4
